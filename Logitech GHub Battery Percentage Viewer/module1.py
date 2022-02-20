8762d728224785e813fd8bcc8c84128cba7ae437
Scruffydrew <ldhe1@ymail.com>
2022-02-20 19:55:45 +1100

    added charge indicator

--- Logitech GHub Battery Percentage Viewer/module1.py
+++
@@ -1,9 +0,0 @@
-import json
-
-print("hi")
-# open output file for reading
-with open('listfile.txt', 'r') as filehandle:
-    list = json.load(filehandle)
-
-print(list)
-input("Press Enter to continue...")
